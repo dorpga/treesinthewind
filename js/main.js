25ba173517bb1a09da3522b31871ff81ce057e58
Carver Harrison <pcarverb@hotmail.com>
2016-10-19 12:28:02 -0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -22,7 +22,7 @@ jQuery(function($, undefined) {
     
     $('#tty').terminal(function(command, term) {
         curmap = map[x + "," + y];
-        term.prompt =  curmap.name + ": ";
+        $('#tty').terminal().prompt =  curmap.name + ": ";
         var cmd = command.split(' ');
         switch (cmd[0]) {
             case "n":
