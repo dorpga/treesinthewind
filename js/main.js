5b327bbda47deedcf6c555652a5cd9c5690099a8
Carver Harrison <pcarverb@hotmail.com>
2016-10-19 11:03:38 -0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -2,12 +2,21 @@ jQuery(function($, undefined) {
     $('#tty').terminal(function(command, term) {
         var cmd = command.split(' ');
         switch (cmd[0]) {
-            case "echo":
-                term.echo(cmd[1]);
+            case "n":
+                break;
+            case "s":
+                break;
+            case "e":
+                break;
+            case "w":
+                break;
+            case "getxy":
+                term.echo("X: " + x);
+                term.echo("Y: " + y);
                 break;
         }
     }, {
-        greetings: 'Terminal.js',
+        greetings: 'Trees in the wind\nCopyright 2016 Carver Harrison',
         name: 'tty',
         height: 720,
         prompt: '# '
